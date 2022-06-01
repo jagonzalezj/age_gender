08e2591cd4242dfa869529017abe2f60fe810f50
Paul Ntouda <peric2005@hotmail.fr>
2022-06-01 14:01:25 +0200

    Get_data function

---
+++ get_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def get_data ():
+    url = "raw_data/age_gender.csv"
+    df = pd.read_csv(url)
+
+    df['pixels'] = df['pixels'].apply(lambda x: np.array(x.split(),dtype='float32'))
